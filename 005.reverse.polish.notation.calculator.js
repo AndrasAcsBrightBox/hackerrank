0af5420fcd8f84475db5b22a625fcce1082d3cf3
András Ács <mr.acs.andras@gmail.com>
2019-04-28 11:37:42 +0200

    Added RPN calculator basic implementation

---
+++ 005.reverse.polish.notation.calculator.js
@@ -0,0 +1,24 @@
+function RPNCalculator(expression) {
+    const operators = ['+', '-', '/', '*'];
+    let calculatorStack = [];
+    const splittedAndCastedExpression = String.prototype.split.call(expression, ' ').map((item) => {
+        let boxed = new Number(item);
+        if (isNaN(boxed)) return item;
+        return boxed.valueOf();
+    });
+    for(let i = 0; i < splittedAndCastedExpression.length; i++) {
+        if(typeof(splittedAndCastedExpression[i]) === "number") {
+            calculatorStack.push(splittedAndCastedExpression[i]);
+        }
+        else if (operators.indexOf(splittedAndCastedExpression[i]) > -1) {
+            const operand1 = calculatorStack.pop();
+            const operand2 = calculatorStack.pop();
+            calculatorStack.push(eval(`(${operand1})${splittedAndCastedExpression[i]}(${operand2})`));
+        }
+    }
+    return calculatorStack.pop();
+}
+
+console.log(RPNCalculator('15 7 1 1 + - / 3 * 2 1 1 + + -'));
+// console.log(RPNCalculator('15 -')); // unary ops
+ // also will need to take into consideration the error scenarios as well
