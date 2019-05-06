84a14c5f4134d10e04b0859eed2e683aa338a14c
András Ács <mr.acs.andras@gmail.com>
2019-05-06 07:00:39 +0200

    multiplication + followup.

---
+++ 012.multiplication.table.js
@@ -0,0 +1,14 @@
+function multiplication() {
+    let outpout = '';
+    for(let i = 1; i < 13; i++) {
+        for(let j = 1; j < 13; j++) {
+
+        }
+    }
+}
+
+// https://sites.google.com/site/steveyegge2/five-essential-phone-screen-questions
+// http://bigocheatsheet.com/
+// http://old.hiredintech.com/app (algorithm and systems design )
+
+// String.prototype.padStart!
