2d075a0cd2b83f91b8c353775811fa0d2476fe61
András Ács <mr.acs.andras@gmail.com>
2019-05-06 20:28:07 +0200

    Multiplication table sorted out.

--- 012.multiplication.table.js
+++ 012.multiplication.table.js
@@ -2,9 +2,11 @@ function multiplication() {
     let outpout = '';
     for(let i = 1; i < 13; i++) {
         for(let j = 1; j < 13; j++) {
-
+            outpout += (i * j).toString().padStart(4, ' ');
         }
+        outpout += '\n';
     }
+    return outpout;
 }
 
 // https://sites.google.com/site/steveyegge2/five-essential-phone-screen-questions
@@ -12,3 +14,5 @@ function multiplication() {
 // http://old.hiredintech.com/app (algorithm and systems design )
 
 // String.prototype.padStart!
+
+console.log(multiplication());
